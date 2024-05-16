e65e35b49dba561ca0b1efe4717197bfbd5e207c
H-ayoul <151735580+H-ayoul@users.noreply.github.com>
2024-05-16 17:22:26 +0200

    Update gsbfrais_bduser.sql
    #12

--- ScriptsSQL/gsbfrais_bduser.sql
+++ ScriptsSQL/gsbfrais_bduser.sql
@@ -1,10 +1,10 @@
 -- création de la base de données
-DROP DATABASE IF EXISTS `gsbfrais`;
-CREATE DATABASE IF NOT EXISTS `gsbfrais`
+DROP DATABASE IF EXISTS `gsbfraisbeta`;
+CREATE DATABASE IF NOT EXISTS `gsbfraisbeta`
   CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- création d'un login et affectation à ce login de tous les droits sur la base 
 -- de données GenDongsbfraisnees
 
 CREATE USER userGsb@'localhost' IDENTIFIED BY 'secret';
-GRANT ALL ON gsbfrais.* TO userGsb@'localhost' ;
+GRANT ALL ON gsbfraisbeta.* TO userGsb@'localhost' ;
